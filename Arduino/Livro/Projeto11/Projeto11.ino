690a6bb7be0c902832567386bb552994d12d5f52
ProfessorDanilo <55631429+ProfessorDanilo@users.noreply.github.com>
2022-03-12 15:11:04 -0300

    continuando com as programações do livro

---
+++ Arduino/Livro/Projeto11/Projeto11.ino
@@ -0,0 +1,68 @@
+//projeto 10 - Mood lamp com controle serial
+char buffer[18];
+int red, green, blue;
+
+int redPin=11, greenPin=10, bluePin=9;
+
+void setup(){
+  Serial.begin(9600);
+  Serial.flush();
+  pinMode(redPin, OUTPUT);
+  pinMode(greenPin, OUTPUT);
+  pinMode(bluePin, OUTPUT);
+}
+
+void loop(){
+  if(Serial.available() > 0){
+    register int index=0;
+    delay(100); //para carregar o buffer
+    int numChar = Serial.available();
+    if(numChar>15){
+      numChar=15;
+    }
+    while(numChar--){
+      buffer[index++] = Serial.read();
+    }
+    splitString(buffer);
+  }
+}
+
+void splitString(char* data){
+  Serial.print("Data entered: ");
+  Serial.println(data);
+  char* parameter;
+  parameter = strtok(data, " ,");
+  while(parameter != NULL){
+    setLED(parameter);
+    parameter = strtok(NULL, " ,");
+  }
+
+  //limpa o texto e os buffer seriais
+  for(int x=0; x<16; x++) buffer[x]='\0';
+
+  Serial.flush();
+}
+
+void setLED(char* data){
+  if((data[0] == 'r') || (data[0] == 'R')){
+    int Ans =strtol(data+1, NULL, 10);
+    Ans = constrain(Ans, 0, 255);
+    analogWrite(redPin, Ans);
+    Serial.print("Red is set to: ");
+    Serial.println(Ans);
+  }
+  if((data[0] == 'g') || (data[0] == 'G')){
+    int Ans =strtol(data+1, NULL, 10);
+    Ans = constrain(Ans, 0, 255);
+    analogWrite(greenPin, Ans);
+    Serial.print("Green is set to: ");
+    Serial.println(Ans);
+  }
+  if((data[0] == 'b') || (data[0] == 'B')){
+    int Ans =strtol(data+1, NULL, 10);
+    Ans = constrain(Ans, 0, 255);
+    analogWrite(bluePin, Ans);
+    Serial.print("Blue is set to: ");
+    Serial.println(Ans);
+  }
+}
